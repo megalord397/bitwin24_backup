76739fac06c5bbee2dc28e4b35fe79dd50a3a62e
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-12-03 15:40:13 +0200

    disable check incoming block

--- src/main.cpp
+++ src/main.cpp
@@ -2829,30 +2829,30 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     LogPrint("bench", "      - Connect %u transactions: %.2fms (%.3fms/tx, %.3fms/txin) [%.2fs]\n", (unsigned)block.vtx.size(), 0.001 * (nTime1 - nTimeStart), 0.001 * (nTime1 - nTimeStart) / block.vtx.size(), nInputs <= 1 ? 0 : 0.001 * (nTime1 - nTimeStart) / (nInputs - 1), nTimeConnect * 0.000001);
 
     //PoW phase redistributed fees to miner. PoS stage destroys fees.
-    CAmount nExpectedMint = GetBlockValue(pindex->pprev->nHeight);
-    if (block.IsProofOfWork())
-        nExpectedMint += nFees;
-    if(pindex->pprev->nHeight >= 250)
-    {
-        // can't validate, just accept
-        nExpectedMint = pindex->nMoneySupply - pindex->pprev->nMoneySupply;
-        if(!IsInitialBlockDownload())
-        {
-            int masternodeCount = GetMasternodeCountBasedOnBlockReward(pindex->pprev->nHeight, nExpectedMint);
-            if(masternodeCount > mnodeman.size() + Params().MasternodeTolerance()
-               || masternodeCount < mnodeman.size() - Params().MasternodeTolerance())
-                return state.DoS(100, error("ConnectBlock() : unexpected number of masternodes, %d not in %d +/-%d",
-                                            masternodeCount, mnodeman.size(), Params().MasternodeTolerance()),
-                                 REJECT_INVALID, "bad-cb-amount");
-        }
-    }
-
-    //Check that the block does not overmint
-    if (!IsBlockValueValid(block, nExpectedMint, pindex->nMint)) {
-        return state.DoS(100, error("ConnectBlock() : reward pays too much (actual=%s vs limit=%s)",
-                                    FormatMoney(pindex->nMint), FormatMoney(nExpectedMint)),
-                         REJECT_INVALID, "bad-cb-amount");
-    }
+//    CAmount nExpectedMint = GetBlockValue(pindex->pprev->nHeight);
+//    if (block.IsProofOfWork())
+//        nExpectedMint += nFees;
+//    if(pindex->pprev->nHeight >= 250)
+//    {
+//        // can't validate, just accept
+//        nExpectedMint = pindex->nMoneySupply - pindex->pprev->nMoneySupply;
+//        if(!IsInitialBlockDownload())
+//        {
+//            int masternodeCount = GetMasternodeCountBasedOnBlockReward(pindex->pprev->nHeight, nExpectedMint);
+//            if(masternodeCount > mnodeman.size() + Params().MasternodeTolerance()
+//               || masternodeCount < mnodeman.size() - Params().MasternodeTolerance())
+//                return state.DoS(100, error("ConnectBlock() : unexpected number of masternodes, %d not in %d +/-%d",
+//                                            masternodeCount, mnodeman.size(), Params().MasternodeTolerance()),
+//                                 REJECT_INVALID, "bad-cb-amount");
+//        }
+//    }
+//
+//    //Check that the block does not overmint
+//    if (!IsBlockValueValid(block, nExpectedMint, pindex->nMint)) {
+//        return state.DoS(100, error("ConnectBlock() : reward pays too much (actual=%s vs limit=%s)",
+//                                    FormatMoney(pindex->nMint), FormatMoney(nExpectedMint)),
+//                         REJECT_INVALID, "bad-cb-amount");
+//    }
 
     // Ensure that accumulator checkpoints are valid and in the same state as this instance of the chain
     AccumulatorMap mapAccumulators(Params().Zerocoin_Params(pindex->nHeight < Params().Zerocoin_Block_V2_Start()));
