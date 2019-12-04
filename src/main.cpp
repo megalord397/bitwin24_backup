ae2c7e5903cb6c0af5746f97c0d2ec66216dc54c
Svyatoslav Delukin <svyatoslav.d@blaize.tech>
2019-12-04 16:49:05 +0200

    add condition for validation of new block, when start clean node

--- src/main.cpp
+++ src/main.cpp
@@ -2842,7 +2842,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     {
         // can't validate, just accept
         nExpectedMint = pindex->nMoneySupply - pindex->pprev->nMoneySupply;
-        if(masternodeSync.IsSynced())
+        if(masternodeSync.IsSynced() && !IsInitialBlockDownload())
         {
             int masternodeCount = GetMasternodeCountBasedOnBlockReward(pindex->pprev->nHeight, nExpectedMint);
             if(masternodeCount >= 0)
